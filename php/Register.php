0eca3e17678e5a187816bd8906ef56c68e58a450
YunaK97 <63200480+YunaK97@users.noreply.github.com>
2020-07-12 14:22:25 +0900

    add login.php & Register.php

---
+++ php/Register.php
@@ -0,0 +1,23 @@
+<?php 
+    $con = mysqli_connect("localhost", "jennyk97", "a2743275!", "jennyk97");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $memID = $_POST["memID"];
+    $memPW = $_POST["memPW"];
+    $memName = $_POST["memName"];
+    $memEmail = $_POST["memEmail"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO MEMBER VALUES (?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "sssi", $memID, $memPW, $memName, $memEmail);
+    mysqli_stmt_execute($statement);
+
+
+    $response = array();
+    $response["success"] = true;
+ 
+   
+    echo json_encode($response);
+
+
+
+?>
