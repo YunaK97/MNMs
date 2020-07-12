f6c48d98571b81de05ba795f579dfc23a78a1089
YunaK97 <63200480+YunaK97@users.noreply.github.com>
2020-07-12 14:18:48 +0900

    add php

---
+++ Login.php
@@ -0,0 +1,31 @@
+<?php
+    $con = mysqli_connect("localhost", "jennyk97", "a2743275!", "jennyk97");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $memID = $_POST["memID"];
+    $memPW = $_POST["memPW"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM MEMBER WHERE memID = ? AND memPW = ?");
+    mysqli_stmt_bind_param($statement, "ss", $memID, $memPW);
+    mysqli_stmt_execute($statement);
+
+
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $memID, $memPW, $memName, $userEmail);
+
+    $response = array();
+    $response["success"] = false;
+ 
+    while(mysqli_stmt_fetch($statement)) {
+        $response["success"] = true;
+        $response["memID"] = $memID;
+        $response["memPW"] = $memPW;
+        $response["memName"] = $memName;
+        $response["memEmail"] = $memEmail;        
+    }
+
+    echo json_encode($response);
+
+
+
+?>
