64c4ab24f3921c18eb42f7a3872450e7cfa75957
HONEY1b <37771348+HONEY1b@users.noreply.github.com>
2020-07-12 21:32:56 +0900

    member.php include 가능

--- php/Login.php
+++ php/Login.php
@@ -1,48 +1,5 @@
 <?php
-class Member {
-
-    // 프로퍼티(멤버 변수)
-	private $memName;
-    private $memID;
-    private $memPW;
-    private $memEmail;
-    private $memSsn;
-	
-    // 메소드
-	public function getMemName() {
-        return $memName;
-    }
-    public function setMemName($memName) {
-        $this->memName = memName;
-    }
-    public function getMemID() {
-        return $memID;
-    }
-    public function setMemID($memID) {
-        $this->memID = memID;
-    }
-    public function getMemPW() {
-        return $memPW;
-    }
-    public function setMemPW($memPW) {
-        $this->memPW = memPW;
-    }
-    public function getMemEmail() {
-        return memEmail;
-    }
-    public function setMemEmail($memEmail) {
-        $this->memEmail = memEmail;
-    }
-    public function getMemSsn() {
-        return $memSsn;
-    }
-    public function setMemSsn($memSsn) {
-        $this->memSsn = memSsn;
-    }
-}
-?>
-
-<?php
+	include $_SERVER['DOCUMENT_ROOT'].'/Member.php';
     $con = mysqli_connect("localhost", "jennyk97", "a2743275!", "jennyk97");
     mysqli_query($con,'SET NAMES utf8');
 
