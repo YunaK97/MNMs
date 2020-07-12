db2f42a2f60376f9abf0a66a7c47718ecd3ee9f8
HONEY1b <37771348+HONEY1b@users.noreply.github.com>
2020-07-12 18:26:52 +0900

    login한 객체 전달

--- php/Login.php
+++ php/Login.php
@@ -1,4 +1,48 @@
 <?php
+class Member {
+
+    // 프로퍼티(멤버 변수)
+	private $memName;
+    private $memID;
+    private $memPW;
+    private $memEmail;
+    private $memSsn;
+	
+    // 메소드
+	public function getMemName() {
+        return $memName;
+    }
+    public function setMemName($memName) {
+        $this->memName = memName;
+    }
+    public function getMemID() {
+        return $memID;
+    }
+    public function setMemID($memID) {
+        $this->memID = memID;
+    }
+    public function getMemPW() {
+        return $memPW;
+    }
+    public function setMemPW($memPW) {
+        $this->memPW = memPW;
+    }
+    public function getMemEmail() {
+        return memEmail;
+    }
+    public function setMemEmail($memEmail) {
+        $this->memEmail = memEmail;
+    }
+    public function getMemSsn() {
+        return $memSsn;
+    }
+    public function setMemSsn($memSsn) {
+        $this->memSsn = memSsn;
+    }
+}
+?>
+
+<?php
     $con = mysqli_connect("localhost", "jennyk97", "a2743275!", "jennyk97");
     mysqli_query($con,'SET NAMES utf8');
 
@@ -14,14 +58,14 @@
     mysqli_stmt_bind_result($statement, $memID, $memPW, $memName, $memEmail);
 
     $response = array();
-    $response["success"] = false;
- 
+	$member=new Member;
+	
     while(mysqli_stmt_fetch($statement)) {
-        $response["success"] = true;
-        $response["memID"] = $memID;
-        $response["memPW"] = $memPW;
-        $response["memName"] = $memName;
-        $response["memEmail"] = $memEmail;        
+		$member->setMemName(memName);
+		$member->setMemID(memID);
+		$member->setMemPW(memPW);
+		$member->setMemEmail(memEmail);
+		$response["member"]=member;
     }
 
     echo json_encode($response);
