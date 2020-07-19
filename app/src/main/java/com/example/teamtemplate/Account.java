ca215114dcb67b504278950a714affe32eacf27d
honey1b <nh822@naver.com>
2020-07-20 00:25:27 +0900

    login complete

---
+++ app/src/main/java/com/example/teamtemplate/Account.java
@@ -0,0 +1,42 @@
+package com.example.teamtemplate;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private String accountNum;
+    private String accountBank;
+    private int accountBalance;
+    private String accountPassword;
+
+    public String getAccountNum() {
+        return accountNum;
+    }
+
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public String getAccountBank() {
+        return accountBank;
+    }
+
+    public void setAccountBank(String accountBank) {
+        this.accountBank = accountBank;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getAccountPassword() {
+        return accountPassword;
+    }
+
+    public void setAccountPassword(String accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+}
