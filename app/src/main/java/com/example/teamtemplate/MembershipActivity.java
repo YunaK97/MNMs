74ba5a893b94c3264d96a7767109db307dc4978e
iingang <ingyung.park08@gmail.com>
2020-07-21 13:28:57 +0900

    그룹방 생성 및 Firebase 연결(진행중) MembershipActivity MembershipAdapter MembershipData activity_membership row_membership

--- app/src/main/java/com/example/teamtemplate/MembershipActivity.java
+++ app/src/main/java/com/example/teamtemplate/MembershipActivity.java
@@ -1,20 +1,84 @@
 package com.example.teamtemplate;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MembershipActivity extends AppCompatActivity {
-    Member loginMember;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<MembershipData> dataList;
+    private String nick = "Server";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membership);
 
-        Intent intent = getIntent();
-        loginMember = (Member) intent.getSerializableExtra("loginMember");
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        dataList = new ArrayList<>();
+        mAdapter = new MembershipAdapter(dataList, MembershipActivity.this, nick);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.setValue("hello world");
+
+        MembershipData mData = new MembershipData();
+        mData.setNickname(nick);
+        mData.setMsg("please2");
+
+        myRef.push().setValue(mData);
+
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //MembershipData mData = snapshot.getValue(MembershipData.class);
+                //((MembershipAdapter) mAdapter).addChat(mData);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/teamtemplate/MembershipAdapter.java
@@ -0,0 +1,77 @@
+package com.example.teamtemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MembershipAdapter extends RecyclerView.Adapter<MembershipAdapter.MyViewHolder> {
+    private List<MembershipData> mDataset;
+    private String myNickName;
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is  just a string in this case
+        public TextView TextView_nickname;
+        public TextView TextView_msg;
+        public View rootView;
+
+        public MyViewHolder(View v) {
+            super(v);
+            TextView_nickname = v.findViewById(R.id.TextView_nickname);
+            TextView_msg = v.findViewById(R.id.TextView_msg);
+            rootView = v;
+
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MembershipAdapter(List<MembershipData> myDataset, Context context, String myNickName) {
+        mDataset = myDataset;
+        this.myNickName = myNickName;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MembershipAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_membership, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        MembershipData mData = mDataset.get(position);
+
+        holder.TextView_nickname.setText(mData.getNickname());
+        holder.TextView_msg.setText(mData.getMsg());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset == null ? 0 : mDataset.size();
+    }
+    public MembershipData getChat(int position) {
+        return mDataset != null ? mDataset.get(position) : null;
+    }
+
+    public void addChat(MembershipData mData) {
+        mDataset.add(mData);
+        notifyItemInserted(mDataset.size()-1);
+    }
+}
