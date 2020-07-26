8236f8c534ba7ff0f16dbb2a0f9ddde45b6658f1
iingang <ingyung.park08@gmail.com>
2020-07-27 01:24:22 +0900

    Merge branch 'master' of /Users/ingyung/AndroidStudioProjects/TeamTemplate with conflicts.

--- app/src/main/java/com/example/teamtemplate/MembershipActivity.java
+++ app/src/main/java/com/example/teamtemplate/MembershipActivity.java
@@ -5,18 +5,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,21 +56,82 @@ public class MembershipActivity extends AppCompatActivity {
         Transaction transact = new Transaction();
         transact.setAccountNum("979796");
 
-
+        //process(transact);
         membershipProcess(transact);
-
     }
+
+    protected void process(final Transaction transact) {
+        final String accountNum = transact.getAccountNum();
+        final String url="http://jennyk97.dothome.co.kr/ListTransaction.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            System.out.println("----------OOO55OOO-----------");
+                            JSONArray j = new JSONArray(response);
+                            System.out.println("----------OOO777OOO-----------");
+                            // Parse json
+                            for (int i = 0; i < j.length(); i++) {
+                                try {
+                                    JSONObject jsonObject = j.getJSONObject(i);
+
+                                    Transaction transact = new Transaction();
+                                    transact.setAccountNum(jsonObject.getString("accountNum"));
+                                    transact.setTransactID(jsonObject.getString("transactID"));
+                                    transact.setTransactHistroy(jsonObject.getString("transactHistory"));
+                                    transact.setTransactMoney(jsonObject.getString("transactMoney"));
+                                    transact.setTransactVersion(jsonObject.getString("transactVersion"));
+                                    transact.setSince(jsonObject.getString("since"));
+
+                                    ((MembershipAdapter) mAdapter).addItem(transact);
+
+                                    System.out.println("----------OOOOOO-----------");
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("********에러********");
+                Log.e("#####볼리에러####", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String >();
+                params.put("accountNum", accountNum);
+                System.out.println("========accountNUM========");
+                return params;
+            }
+        };
+
+        RequestQueue queue= Volley.newRequestQueue(MembershipActivity.this);
+        queue.add(stringRequest);
+    }
+
 
     protected void membershipProcess(final Transaction transact){
 
         final String accountNum = transact.getAccountNum();
-        String serverUrl="http://jennyk97.dothome.co.kr/ListTransaction.php";
+        final String url="http://jennyk97.dothome.co.kr/ListTransaction.php";
 
         //결과를 JsonArray 받을 것이므로..
         //StringRequest가 아니라..
         //JsonArrayRequest를 이용할 것임
-        final JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(Request.Method.POST, serverUrl, null, new Response.Listener<JSONArray>() {
+
+        final JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(Request.Method.POST, url,  null, new Response.Listener<JSONArray>() {
             //volley 라이브러리의 GET방식은 버튼 누를때마다 새로운 갱신 데이터를 불러들이지 않음. 그래서 POST 방식 사용
+
             @Override
             public void onResponse(JSONArray response) {
                 try {
@@ -89,17 +160,10 @@ public class MembershipActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                System.out.println("********에러********");
+                Log.e("#####볼리에러####", error.toString());
             }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> map = new HashMap<>();
-                map.put("accountNum", accountNum);
-
-                return map;
-            }
-
-        };
+        });
 
         RequestQueue queue= Volley.newRequestQueue(MembershipActivity.this);
         queue.add(jsonArrayRequest);
