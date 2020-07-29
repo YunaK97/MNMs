5b4e1636002e36addd22b939750f95223a93bbd8
iingang <ingyung.park08@gmail.com>
2020-07-29 16:33:02 +0900

    DailyActivity DB연결 성공

--- app/src/main/java/com/example/teamtemplate/daily/DailyActivity.java
+++ app/src/main/java/com/example/teamtemplate/daily/DailyActivity.java
@@ -1,11 +1,16 @@
 package com.example.teamtemplate.daily;
 
-import android.os.Bundle;
-import android.util.Log;
-
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.os.health.SystemHealthManager;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,7 +18,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.teamtemplate.Member;
 import com.example.teamtemplate.R;
+import com.example.teamtemplate.daily.DailyGroup;
 import com.example.teamtemplate.transaction.Transaction;
 import com.example.teamtemplate.transaction.TransactionAdapter;
 
@@ -26,17 +33,44 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class DailyActivity extends AppCompatActivity {
+    public RadioButton rbt_up;
+    public RadioButton rbt_round;
+    public RadioButton rbt_down;
+    public RadioGroup radioGroup;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<Transaction> dataList;
 
+    DailyGroup dailyGroup = new DailyGroup();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily);
+
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        rbt_up = findViewById(R.id.rbt_up);
+        rbt_round = findViewById(R.id.rbt_round);
+        rbt_down = findViewById(R.id.rbt_down);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int i) {
+                if(i == R.id.rbt_up) {
+                    dailyGroup.setDutchPay("up");
+                }
+                else if(i== R.id.rbt_round){
+                    dailyGroup.setDutchPay("round");
+                }
+                else if(i == R.id.rbt_down) {
+                    dailyGroup.setDutchPay("down");
+            }
+        }
+    });
 
         mRecyclerView = findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -48,9 +82,11 @@ public class DailyActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
         Transaction transact = new Transaction();
-        transact.setAccountNum("979796");
-
+        transact.setAccountNum("1010");
         transactionProcess(transact);
+
+        dailyGroup.setDID("D1");
+        dailyProcess(dailyGroup);
     }
 
     protected void transactionProcess(final Transaction transact) {
@@ -75,6 +111,7 @@ public class DailyActivity extends AppCompatActivity {
                             transact.setTransactMoney(jsonObject.getString("transactMoney"));
                             transact.setTransactVersion(jsonObject.getString("transactVersion"));
                             transact.setSince(jsonObject.getString("since"));
+                            transact.setMID(jsonObject.getString("MID"));
 
                             ((TransactionAdapter) mAdapter).addItem(transact);
 
@@ -100,15 +137,63 @@ public class DailyActivity extends AppCompatActivity {
         }){
             @Override
             protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String >();
+                Map<String, String> params = new HashMap<>();
                 params.put("accountNum", accountNum);
                 System.out.println("========accountNUM========");
                 return params;
             }
         };
 
-        RequestQueue queue= Volley.newRequestQueue( DailyActivity.this);
+        RequestQueue queue= Volley.newRequestQueue(DailyActivity.this);
         queue.add(stringRequest);
     }
+
+    protected void dailyProcess(final DailyGroup dailyGroup) {
+        final String DID = dailyGroup.getDID();
+        final String dutchPay = dailyGroup.getDutchPay();
+        final String url="http://jennyk97.dothome.co.kr/DailyGroup.php";
+
+        StringRequest stringRequest2 = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("11111111111");
+                try {
+
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    DailyGroup dg = new DailyGroup();
+                    dg.setDID(jsonObject.getString("DID"));
+                    dg.setMoney(jsonObject.getString("money"));
+                    dg.setDutchPay(jsonObject.getString("dutchPay"));
+                    dg.setGID(jsonObject.getString("GID"));
+
+                    System.out.println("----------OOO222OOO-----------");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("********에러********");
+                Log.e("#####볼리에러####", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("dutchPay", dutchPay);
+                params.put("DID", DID);
+                System.out.println("========dutchPay DID========");
+                return params;
+            }
+        };
+        RequestQueue queue= Volley.newRequestQueue(DailyActivity.this);
+        queue.add(stringRequest2);
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/teamtemplate/daily/DailyGroup.java
@@ -0,0 +1,41 @@
+package com.example.teamtemplate.daily;
+
+public class DailyGroup {
+    String DID;
+    String money;
+    String dutchPay;
+    String GID;
+
+    public String getDID() {
+        return DID;
+    }
+
+    public void setDID(String DID) {
+        this.DID = DID;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    public String getDutchPay() {
+        return dutchPay;
+    }
+
+    public void setDutchPay(String dutchPay) {
+        this.dutchPay = dutchPay;
+    }
+
+    public String getGID() {
+        return GID;
+    }
+
+    public void setGID(String GID) {
+        this.GID = GID;
+    }
+
+}
