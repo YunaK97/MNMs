dd1f0b397014e499fad3fae5ac872c1449ca9a20
Honey1b <nh822@naver.com>
2020-09-27 18:03:54 +0900

    친구 추가 완성 멤버십 추가 구현 중 멤버십 관련 transaction 구현 중

--- app/src/main/java/kr/hongik/mnms/membership/ui/home/NewFeeActivity.java
+++ app/src/main/java/kr/hongik/mnms/membership/ui/home/NewFeeActivity.java
@@ -10,5 +10,6 @@ public class NewFeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_fee);
+
     }
 }
